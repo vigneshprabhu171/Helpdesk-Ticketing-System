ae98645aba4b1f3e147362e7f0bebc0b2cf15e18
vigneshprabhu171 <vigneshprabhu171@gmail.com>
2026-06-11 00:45:53 +0530

    Fix CORS for Netlify

--- backend/src/main/java/helpdesk/config/CorsConfig.java
+++ backend/src/main/java/helpdesk/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:5173",
+                        "https://helpdesktktsys.netlify.app")
         );
 
         configuration.setAllowedMethods(
